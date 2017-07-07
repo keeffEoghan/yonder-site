b2d251d632a9954816aac9dd8c0c6de2d691c7b6
Eoghan O'Keeffe <eoghanokeeffe@Eoghans-MBP.lan>
2017-07-07 17:05:47 +0100

    Squarespace configuration and debugging

---
+++ scripts/constants.js
@@ -0,0 +1,2 @@
+export const authenticated = document.documentElement.getAttribute('data-authenticated-account') === '';
+export const debug = false;
--- scripts/site.js
+++ scripts/site.js
@@ -5,16 +5,19 @@
 //
 // http://github.com/squarespace/squarespace-core
 
-var core = require('@squarespace/core');
+import core from '@squarespace/core';
+import controller from '@squarespace/controller';
+
+import siteLoader from './controllers/site-loader';
+
+controller.register('site-loader', siteLoader);
 
 window.addEventListener('DOMContentLoaded', function() {
+    var images = document.querySelectorAll('img[data-src]');
 
-  var images = document.querySelectorAll('img[data-src]');
-
-  for (var i = 0; i < images.length; i++) {
-    core.ImageLoader.load(images[i], {
-      load: true
-    });
-  }
-
+    for(var i = 0; i < images.length; i++) {
+        core.ImageLoader.load(images[i], {
+            load: true
+        });
+    }
 });
